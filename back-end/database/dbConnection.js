f50d3e5fdb0a29279af33dee8688ab27bc798f76
Savan Rathod <savansr26@gmail.com>
2024-07-23 21:17:24 +0530

    .

--- back-end/database/dbConnection.js
+++ back-end/database/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const dbConnection=()=>{
      try{
-        mongoose.connect('mongodb+srv://savansr26:vNkPLR2JNXc9zWvp@cluster-1.jby1wf8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1',{dbName:'VolunteeringProject'});
+        mongoose.connect("mongodb+srv://savansr26:vNkPLR2JNXc9zWvp@cluster-1.jby1wf8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1",{dbName:'VolunteeringProject'});
         console.log('Connected To DataBase');
      }catch(error){
         console.log('Failed To Connect To DataBase');
