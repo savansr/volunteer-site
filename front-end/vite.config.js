66cab4867e2bff3fffbf213f6efd687d8f2a01ec
savansr <Savansr26@gmail.com>
2025-03-19 22:00:50 +0530

    Update vite.config.js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'],
-  },
   build: {
     rollupOptions: {
-      external: [],
+      external: ['@clerk/clerk-react'],
     },
   },
+ 
 })
