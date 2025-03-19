aff0b56c537a329f33cf4c5dacc4e7dab08d49b3
savansr <Savansr26@gmail.com>
2025-03-19 19:15:08 +0530

    Update main.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
-import { ClerkProvider } from '@clerk/clerk-react'
+const { ClerkProvider }=window.Clerk;
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
