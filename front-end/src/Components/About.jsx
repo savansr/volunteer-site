a1dba0fcb091c25858b1ef8dce617d1fec931ccc
savansr <Savansr26@gmail.com>
2025-04-04 04:55:23 +0530

    Update About.jsx

--- front-end/src/Components/About.jsx
+++ front-end/src/Components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
           <div className="hero">
             <div className="banner">
               <h1>Our Story</h1>
-              <p>Welcome to [Your Organization Name], where we've just begun our journey of making a positive impact through volunteerism and generosity. Founded recently, our mission is simple yet powerful: to unite individuals who are passionate about creating change. Whether it's participating in local initiatives or contributing to important causes through donations, every action counts.
+              <p>Welcome to our community, where we've just begun our journey of making a positive impact through volunteerism and generosity. Founded recently, our mission is simple yet powerful: to unite individuals who are passionate about creating change. Whether it's participating in local initiatives or contributing to important causes through donations, every action counts.
 
 Together, we're building a community driven by compassion and a shared vision for a better tomorrow. As we grow, our commitment remains steadfast in empowering communities and inspiring hope. Join us as we write the first chapters of our story, where each volunteer and supporter plays a crucial role in making a meaningful difference.</p>
             </div>
